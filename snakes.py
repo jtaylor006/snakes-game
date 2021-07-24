a4d07f67a08d63c7880e393118fcc090ef4fe947
jtaylor <jarrede20@gmail.com>
2021-07-24 13:53:10 -0400

    began creating the snake

--- snakes.py
+++ snakes.py
@@ -1,14 +1,19 @@
 import pygame
-
 pygame.init()
-dis=pygame.display.set_mode((400,300))
-pygame.display.update()
+dis = pygame.display.set_mode((400, 300))
 
-pygame.display.set_caption('Snake game by Jarred')
-game_over=False
+pygame.display.set_caption('Snakes Game by Jarred')
+
+blue=(0,0,225)
+red=(225,0,0)
+
+game_over = False
 while not game_over:
-    for even in pygame.event.get():
-        print(event) #prints all of the actions that takes place
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        pygame.draw.rect(dis,blue,[200,150,10,10])
+        pygame.display.update()
 
 pygame.quit()
 quit()
